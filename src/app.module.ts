9edd28c4e9697f62a9b5910892ce23facc2626fa
kcarww <carlos.oe.cc@gmail.com>
2025-03-27 22:53:52 +0000

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { SpotsModule } from './spots/spots.module';
+
+@Module({
+  imports: [EventsModule, PrismaModule, SpotsModule],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
